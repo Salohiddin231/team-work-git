5ad125363d59dc35db4d638fe2016ef8515599b3
Salohiddin231 <davletovsalohiddin231@gmail.com>
2025-05-18 16:53:41 +0300

    update_work_end

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,11 +1,11 @@
 
-export function Info({num, text}) {
+export function Info({ num, text1, text2, text3 }) {
   return (
     <>
-          <div className="info_group">
-                <h3 className="info_num">{num}</h3>
-                <p className="info_text">{text}</p>
-            </div>
+      <div className="info_group">
+        <h3 className="info_num">{num}</h3>
+        <p className="info_text">{text1} <span>{text2}</span> {text3}</p>
+      </div>
     </>
   )
 }
